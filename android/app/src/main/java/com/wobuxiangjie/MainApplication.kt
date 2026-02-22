b508d9a8b16b0d505c9a3d2b60ec1c9304dff9e8
Xcui3 <120148977+Xcui3@users.noreply.github.com>
2026-02-22 20:57:26 +0800

    fix: Hermes ??????

--- android/app/src/main/java/com/wobuxiangjie/MainApplication.kt
+++ android/app/src/main/java/com/wobuxiangjie/MainApplication.kt
@@ -5,24 +5,17 @@ import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
 import com.facebook.react.ReactHost
 import com.facebook.react.ReactNativeApplicationEntryPoint.loadReactNative
-import com.facebook.react.ReactNativeHost
-import com.facebook.react.ReactPackage
-import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 
 class MainApplication : Application(), ReactApplication {
 
-  private val mReactNativeHost: ReactNativeHost =
-    object : DefaultReactNativeHost(this) {
-      override fun getUseDeveloperSupport(): Boolean = false
-      override fun getPackages(): List<ReactPackage> =
-        PackageList(this).packages.apply {}
-      override fun getJSMainModuleName(): String = "index"
-      override val isHermesEnabled: Boolean = false
-    }
-
-  override val reactHost: ReactHost
-    get() = getDefaultReactHost(applicationContext, mReactNativeHost)
+  override val reactHost: ReactHost by lazy {
+    getDefaultReactHost(
+      context = applicationContext,
+      packageList =
+        PackageList(this).packages.apply {},
+    )
+  }
 
   override fun onCreate() {
     super.onCreate()
